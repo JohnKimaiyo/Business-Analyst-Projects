256ec8942fd36e6772691c79f64aa52fef938cfa
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-19 13:04:35 +0300

    import python libraries

---
+++ keams.py
@@ -0,0 +1,8 @@
+# import libraries
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt 
+from sklearn.cluster import KMeans 
+sns.set()
+from sklearn  import preprocessing
