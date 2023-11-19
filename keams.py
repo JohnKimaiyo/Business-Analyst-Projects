68bc8815d8696f75fcc4a24a8fa58eec42e01c84
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-19 13:30:17 +0300

    final commit

--- keams.py
+++ keams.py
@@ -6,3 +6,8 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans 
 sns.set()
 from sklearn  import preprocessing
+
+
+import data
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00352/Online%20Retail.xlsx"
+data = pd.read_excel(url)
