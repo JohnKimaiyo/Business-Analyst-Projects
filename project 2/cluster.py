bbb1e748ce4cfb98d57979c4c7bcc8122b406d71
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-20 10:34:32 +0300

    final commit

---
+++ project 2/cluster.py
@@ -0,0 +1,14 @@
+# K Means Clustering for Customer Data
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import plotly as py
+import plotly.graph_objs as go
+
+from sklearn.cluster import KMeans
+
+
+import warnings
+warnings.filterwarnings("ignore")
