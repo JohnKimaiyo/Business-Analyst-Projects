45c38e0903508c5324e943238b66a8810ea31bfd
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-22 11:58:02 +0300

    final commit

--- Basics.py
+++ Basics.py
@@ -15,3 +15,12 @@ print(A[:, :2])
 
 a = np.arange(5)
 a + 20
+
+import pandas as pd
+
+certificates_earned = pd.Series(
+    [8, 2, 5, 6],
+    index=['Tom', 'Kris', 'Ahmad', 'Beau']
+)
+
+print(certificates_earned)
