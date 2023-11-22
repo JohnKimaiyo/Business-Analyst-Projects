f044e1dac275e0d2a120628d8d1b0bd7ac71615a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-22 12:03:37 +0300

    final commit

--- Basics.py
+++ Basics.py
@@ -33,3 +33,14 @@ certificates_earned = pd.Series(
 )
 
 print(certificates_earned[certificates_earned > 5])
+
+import pandas as pd
+
+certificates_earned = pd.DataFrame({
+    'Certificates': [8, 2, 5, 6],
+    'Time (in months)': [16, 5, 9, 12]
+})
+
+certificates_earned.index = ['Tom', 'Kris', 'Ahmad', 'Beau']
+
+print(certificates_earned.iloc[2])
