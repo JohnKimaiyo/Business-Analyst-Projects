cdfebd78f05240582fc8d034620d5e6080d8744a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-24 13:11:43 +0300

    final commit

--- w3schools/numpy basic/array join/1 example.py
+++ w3schools/numpy basic/array join/1 example.py
@@ -8,3 +8,14 @@ arr2 = np.array([4, 5, 6])
 arr = np.concatenate((arr1, arr2))
 
 print(arr)
+
+# example 2
+import numpy as np
+
+arr1 = np.array([[1, 2], [3, 4]])
+
+arr2 = np.array([[5, 6], [7, 8]])
+
+arr = np.concatenate((arr1, arr2), axis=1)
+
+print(arr)
