68069916167a3ef62dbab8b9994e5d739afea9b1
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-24 12:57:34 +0300

    final commit

--- w3schools/numpy basic/nupy iterating/1 example.py
+++ w3schools/numpy basic/nupy iterating/1 example.py
@@ -65,3 +65,10 @@ arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
 
 for x in np.nditer(arr[:, ::2]):
   print(x)
+  # exmampl e 8
+import numpy as np
+
+arr = np.array([1, 2, 3])
+
+for idx, x in np.ndenumerate(arr):
+  print(idx, x)
