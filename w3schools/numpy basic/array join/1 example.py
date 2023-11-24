cc49f351b821457cd7f6da8f64acda7a871378e0
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-24 13:25:46 +0300

    final commit

--- w3schools/numpy basic/array join/1 example.py
+++ w3schools/numpy basic/array join/1 example.py
@@ -41,3 +41,15 @@ arr2 = np.array([4, 5, 6])
 arr = np.hstack((arr1, arr2))
 
 print(arr)
+
+
+# example 5
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+
+arr2 = np.array([4, 5, 6])
+
+arr = np.vstack((arr1, arr2))
+
+print(arr)
