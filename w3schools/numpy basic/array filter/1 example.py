c079f8c1b15810e18680faa6026dc998923675df
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-24 13:59:15 +0300

    final commit

--- w3schools/numpy basic/array filter/1 example.py
+++ w3schools/numpy basic/array filter/1 example.py
@@ -29,3 +29,24 @@ newarr = arr[filter_arr]
 
 print(filter_arr)
 print(newarr)
+
+# example 3
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+# Create an empty list
+filter_arr = []
+
+# go through each element in arr
+for element in arr:
+  # if the element is completely divisble by 2, set the value to True, otherwise False
+  if element % 2 == 0:
+    filter_arr.append(True)
+  else:
+    filter_arr.append(False)
+
+newarr = arr[filter_arr]
+
+print(filter_arr)
+print(newarr)
