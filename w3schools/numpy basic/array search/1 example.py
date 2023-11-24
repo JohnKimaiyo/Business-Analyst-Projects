e7bca6b5ddb65cd16ddd67054538fd6743e3dc8d
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-24 13:48:27 +0300

    final commit

--- w3schools/numpy basic/array search/1 example.py
+++ w3schools/numpy basic/array search/1 example.py
@@ -42,3 +42,13 @@ arr = np.array([6, 7, 8, 9])
 x = np.searchsorted(arr, 7, side='right')
 
 print(x)
+
+# example 6
+
+import numpy as np
+
+arr = np.array([1, 3, 5, 7])
+
+x = np.searchsorted(arr, [2, 4, 6])
+
+print(x)
