4b80e08a657d584ab97a7228632cc70f395fb859
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-24 09:37:12 +0300

    final commit

---
+++ w3schools/numpy basic/array indexing/1 Exxample.py
@@ -0,0 +1,13 @@
+# example 1
+import numpy as np
+
+arr = np.array([1, 2, 3, 4,5,6,7,8,9,10])
+
+print(arr[0])
+
+# example 2
+import numpy as np
+
+arr = np.array([1, 2, 3, 4,5,6,7,8,9,10])
+
+print(arr[1])
