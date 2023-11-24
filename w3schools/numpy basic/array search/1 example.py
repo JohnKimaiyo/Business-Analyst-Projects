41d4fe7901426de9090f7df4200cdac5f6f2e5fe
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-24 13:44:38 +0300

    final commir

--- w3schools/numpy basic/array search/1 example.py
+++ w3schools/numpy basic/array search/1 example.py
@@ -24,3 +24,12 @@ arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
 x = np.where(arr%2 == 1)
 
 print(x)
+
+# example 4
+import numpy as np
+
+arr = np.array([6, 7, 8, 9])
+
+x = np.searchsorted(arr, 7)
+
+print(x)
