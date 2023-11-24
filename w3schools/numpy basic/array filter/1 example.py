2e2afd9416a75caccafe1f9a567a3e309940afe7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-24 13:59:55 +0300

    final commit

--- w3schools/numpy basic/array filter/1 example.py
+++ w3schools/numpy basic/array filter/1 example.py
@@ -50,3 +50,15 @@ newarr = arr[filter_arr]
 
 print(filter_arr)
 print(newarr)
+
+#example 3
+import numpy as np
+
+arr = np.array([41, 42, 43, 44])
+
+filter_arr = arr > 42
+
+newarr = arr[filter_arr]
+
+print(filter_arr)
+print(newarr)
