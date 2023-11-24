b64ee0277ca5bc8b596c133e8a40f307048bab6d
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-24 11:53:48 +0300

    final commit

--- w3schools/numpy basic/array reshape/1 example.py
+++ w3schools/numpy basic/array reshape/1 example.py
@@ -42,3 +42,11 @@ arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
 newarr = arr.reshape(2, 2, -1)
 
 print(newarr)
+
+# import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+newarr = arr.reshape(-1)
+
+print(newarr)
