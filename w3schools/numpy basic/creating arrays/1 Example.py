e5cb45104e8a88169a1365be9c82ad038ebcef82
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-24 09:17:38 +0300

    final commit

--- w3schools/numpy basic/creating arrays/1 Example.py
+++ w3schools/numpy basic/creating arrays/1 Example.py
@@ -6,11 +6,21 @@ print(arr)
 
 print(type(arr))
 
-# example 1
+# example 2
 
 import numpy as np
 
 arr = np.array([2, 3, 4, 5, 6])
+
+print(arr)
+
+print(type(arr))
+
+# example 3
+
+import numpy as np
+
+arr = np.array([3,4, 5, 6, 7])
 
 print(arr)
 
