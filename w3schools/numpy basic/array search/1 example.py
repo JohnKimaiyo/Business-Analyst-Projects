5794d187640f2f1dc1d8f22b5fcc1c968198bcdb
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-24 13:43:35 +0300

    final commit

--- w3schools/numpy basic/array search/1 example.py
+++ w3schools/numpy basic/array search/1 example.py
@@ -15,3 +15,12 @@ arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
 x = np.where(arr%2 == 0)
 
 print(x)
+
+# examplme 3
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+
+x = np.where(arr%2 == 1)
+
+print(x)
