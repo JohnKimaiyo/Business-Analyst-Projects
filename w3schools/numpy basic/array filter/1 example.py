f535a6589b862ee1dfa2c02b41517006ea70ac7f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-24 13:57:18 +0300

    final commit

---
+++ w3schools/numpy basic/array filter/1 example.py
@@ -0,0 +1,10 @@
+# example 1
+import numpy as np
+
+arr = np.array([41, 42, 43, 44])
+
+x = [True, False, True, False]
+
+newarr = arr[x]
+
+print(newarr)
--- w3schools/numpy basic/array sort/1 example.py
+++ w3schools/numpy basic/array sort/1 example.py
@@ -19,3 +19,10 @@ import numpy as np
 arr = np.array([True, False, True])
 
 print(np.sort(arr))
+
+# example 4
+import numpy as np
+
+arr = np.array([[3, 2, 4], [5, 0, 1]])
+
+print(np.sort(arr))
