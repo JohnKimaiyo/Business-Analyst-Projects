89d7461840f5af4823e049b3d7014f6c052f6018
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-24 12:56:00 +0300

    final commit

--- w3schools/numpy basic/nupy iterating/1 example.py
+++ w3schools/numpy basic/nupy iterating/1 example.py
@@ -49,3 +49,13 @@ arr = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
 
 for x in np.nditer(arr):
   print(x)
+
+  # example 7 
+  import numpy as np
+
+arr = np.array([1, 2, 3])
+
+for x in np.nditer(arr, flags=['buffered'], op_dtypes=['S']):
+  print(x)
+
+  
