af51c1b5ca50c208e91494e6e7fdd5b95fbd758e
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-24 12:51:10 +0300

    final commit

--- w3schools/numpy basic/nupy iterating/1 example.py
+++ w3schools/numpy basic/nupy iterating/1 example.py
@@ -13,3 +13,12 @@ arr = np.array([[1, 2, 3], [4, 5, 6]])
 
 for x in arr:
   print(x)
+
+# example 3
+  import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+for x in arr:
+  for y in x:
+    print(y)
