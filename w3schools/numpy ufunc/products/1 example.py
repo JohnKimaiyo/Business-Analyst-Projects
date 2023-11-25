5711360966e25bfe0bbe8e1f13728102850fc179
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 18:03:20 +0300

    final commit

--- w3schools/numpy ufunc/products/1 example.py
+++ w3schools/numpy ufunc/products/1 example.py
@@ -6,3 +6,13 @@ arr = np.array([1, 2, 3, 4])
 x = np.prod(arr)
 
 print(x)
+
+# exxample 2
+import numpy as np
+
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([5, 6, 7, 8])
+
+x = np.prod([arr1, arr2])
+
+print(x)
