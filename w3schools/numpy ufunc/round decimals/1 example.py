6bb89991c1a85216536e5ff8f073199879c8a9db
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 13:28:14 +0300

    final commit

--- w3schools/numpy ufunc/round decimals/1 example.py
+++ w3schools/numpy ufunc/round decimals/1 example.py
@@ -11,3 +11,17 @@ import numpy as np
 arr = np.fix([-3.1666, 3.6667])
 
 print(arr)
+
+# example 3
+import numpy as np
+
+arr = np.around(3.1666, 2)
+
+print(arr)
+
+# example 4
+import numpy as np
+
+arr = np.floor([-3.1666, 3.6667])
+
+print(arr)
