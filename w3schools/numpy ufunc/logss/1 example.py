9eeabb1bd2c525ce7773815bc94d332d7fc81684
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 17:50:31 +0300

    final commit

--- w3schools/numpy ufunc/logss/1 example.py
+++ w3schools/numpy ufunc/logss/1 example.py
@@ -11,3 +11,11 @@ import numpy as np
 arr = np.arange(1, 10)
 
 print(np.log10(arr))
+
+# example 3
+from math import log
+import numpy as np
+
+nplog = np.frompyfunc(log, 2, 1)
+
+print(nplog(100, 15))
