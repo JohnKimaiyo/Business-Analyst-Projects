27ca6701ab72d6a13ef09ea6a00116347d3ebcf4
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 12:34:17 +0300

    final commit

--- w3schools/numpy randon/pareton distribution/1 example.py
+++ w3schools/numpy randon/pareton distribution/1 example.py
@@ -4,3 +4,12 @@ from numpy import random
 x = random.pareto(a=2, size=(2, 3))
 
 print(x)
+
+# example 2
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.pareto(a=2, size=1000), kde=False)
+
+plt.show()
