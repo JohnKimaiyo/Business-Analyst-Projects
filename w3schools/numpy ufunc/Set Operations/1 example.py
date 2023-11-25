4d65493a527d68cd5958ce0dcbd5738a9bc7f26b
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 18:55:13 +0300

    final commit

--- w3schools/numpy ufunc/Set Operations/1 example.py
+++ w3schools/numpy ufunc/Set Operations/1 example.py
@@ -26,3 +26,13 @@ arr2 = np.array([3, 4, 5, 6])
 newarr = np.intersect1d(arr1, arr2, assume_unique=True)
 
 print(newarr)
+
+# example 4
+import numpy as np
+
+set1 = np.array([1, 2, 3, 4])
+set2 = np.array([3, 4, 5, 6])
+
+newarr = np.setdiff1d(set1, set2, assume_unique=True)
+
+print(newarr)
