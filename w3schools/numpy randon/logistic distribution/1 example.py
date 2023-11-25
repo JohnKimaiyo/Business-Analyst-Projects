1392e0762baa62e0cdafeb7098c07a57c67c890d
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 12:19:39 +0300

    final commit

--- w3schools/numpy randon/logistic distribution/1 example.py
+++ w3schools/numpy randon/logistic distribution/1 example.py
@@ -6,3 +6,12 @@ import seaborn as sns
 sns.distplot(random.uniform(size=1000), hist=False)
 
 plt.show()
+
+# example 2
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.logistic(size=1000), hist=False)
+
+plt.show()
