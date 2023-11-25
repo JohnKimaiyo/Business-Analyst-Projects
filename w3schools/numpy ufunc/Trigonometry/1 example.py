53c951b285a8309e9a0214b89ac588a97264f02d
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 18:31:04 +0300

    final commit

--- w3schools/numpy ufunc/Trigonometry/1 example.py
+++ w3schools/numpy ufunc/Trigonometry/1 example.py
@@ -22,3 +22,12 @@ arr = np.array([90, 180, 270, 360])
 x = np.deg2rad(arr)
 
 print(x)
+
+# example 4
+import numpy as np
+
+arr = np.array([np.pi/2, np.pi, 1.5*np.pi, 2*np.pi])
+
+x = np.rad2deg(arr)
+
+print(x)
