ef4aab2e8e587435c5996d1af1a578ad0ef3047f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 18:53:00 +0300

    final commir

--- w3schools/numpy ufunc/Set Operations/1 example.py
+++ w3schools/numpy ufunc/Set Operations/1 example.py
@@ -6,3 +6,13 @@ arr = np.array([1, 1, 1, 2, 3, 4, 5, 5, 6, 7])
 x = np.unique(arr)
 
 print(x)
+
+# example 2
+import numpy as np
+
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([3, 4, 5, 6])
+
+newarr = np.union1d(arr1, arr2)
+
+print(newarr)
