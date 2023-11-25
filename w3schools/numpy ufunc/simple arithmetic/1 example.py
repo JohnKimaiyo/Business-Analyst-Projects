081be2ae23a128b6fe7658d39b4744388c5bba12
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 13:15:06 +0300

    final commit

--- w3schools/numpy ufunc/simple arithmetic/1 example.py
+++ w3schools/numpy ufunc/simple arithmetic/1 example.py
@@ -38,3 +38,14 @@ newarr = np.divide(arr1, arr2)
 
 print(newarr)
 
+
+# example 4
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40, 50, 60])
+arr2 = np.array([3, 5, 6, 8, 2, 33])
+
+newarr = np.power(arr1, arr2)
+
+print(newarr)
+
