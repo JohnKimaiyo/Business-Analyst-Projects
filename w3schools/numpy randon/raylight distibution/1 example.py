dcc14d9240d8de90c8c3f9bd88dabd21aab2e3d5
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 12:31:55 +0300

    final commit

--- w3schools/numpy randon/raylight distibution/1 example.py
+++ w3schools/numpy randon/raylight distibution/1 example.py
@@ -4,3 +4,12 @@ from numpy import random
 x = random.rayleigh(scale=2, size=(2, 3))
 
 print(x)
+
+# example 2
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.rayleigh(size=1000), hist=False)
+
+plt.show()
