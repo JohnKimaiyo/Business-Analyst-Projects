f90dcf5377ce5e3054b8177493539f08c933a9db
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 19:23:54 +0300

    final commit

--- w3schools/pandas/DataFrame/1 example.py
+++ w3schools/pandas/DataFrame/1 example.py
@@ -10,3 +10,15 @@ data = {
 df = pd.DataFrame(data)
 
 print(df) 
+
+# example 2
+import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+
+print(df)
