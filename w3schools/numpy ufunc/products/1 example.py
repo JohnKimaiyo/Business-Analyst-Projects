b576500e14eb6163e125133311b218b8a6ccb17c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 18:08:00 +0300

    final commit

--- w3schools/numpy ufunc/products/1 example.py
+++ w3schools/numpy ufunc/products/1 example.py
@@ -26,3 +26,12 @@ arr2 = np.array([5, 6, 7, 8])
 newarr = np.prod([arr1, arr2], axis=1)
 
 print(newarr)
+
+# example 4
+import numpy as np
+
+arr = np.array([5, 6, 7, 8])
+
+newarr = np.cumprod(arr)
+
+print(newarr)
