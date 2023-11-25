acd74f6f7cb75ee61b3a07ec50571b3ca4e87a4e
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 17:58:52 +0300

    final commit

--- w3schools/numpy ufunc/logss/1 example.py
+++ w3schools/numpy ufunc/logss/1 example.py
@@ -49,3 +49,12 @@ arr2 = np.array([1, 2, 3])
 newarr = np.sum([arr1, arr2], axis=1)
 
 print(newarr)
+
+# example 5
+import numpy as np
+
+arr = np.array([1, 2, 3])
+
+newarr = np.cumsum(arr)
+
+print(newarr)
