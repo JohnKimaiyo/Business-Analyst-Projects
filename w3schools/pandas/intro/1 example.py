7b38a424f7ce69ae5b3da3d60f460902b3580c72
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 19:08:07 +0300

    final commit

--- w3schools/pandas/intro/1 example.py
+++ w3schools/pandas/intro/1 example.py
@@ -9,3 +9,15 @@ mydataset = {
 myvar = pandas.DataFrame(mydataset)
 
 print(myvar)
+
+# example 2
+import pandas as pd
+
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford"],
+  'passings': [3, 7, 2]
+}
+
+myvar = pd.DataFrame(mydataset)
+
+print(myvar)
