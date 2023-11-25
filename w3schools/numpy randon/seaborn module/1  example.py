e594a272ccce7533c7fe07b5b582d681602e961b
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 11:05:24 +0300

    final commit

--- w3schools/numpy randon/seaborn module/1  example.py
+++ w3schools/numpy randon/seaborn module/1  example.py
@@ -5,3 +5,11 @@ import seaborn as sns
 sns.distplot([0, 1, 2, 3, 4, 5])
 
 plt.show()
+
+# example 2
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot([0, 1, 2, 3, 4, 5], hist=False)
+
+plt.show()
