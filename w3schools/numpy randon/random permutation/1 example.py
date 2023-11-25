9b620114be46c8d0ef75c26203a1ceb342bd6568
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 10:56:40 +0300

    final commit

--- w3schools/numpy randon/random permutation/1 example.py
+++ w3schools/numpy randon/random permutation/1 example.py
@@ -7,3 +7,11 @@ arr = np.array([1, 2, 3, 4, 5])
 random.shuffle(arr)
 
 print(arr)
+
+# example 2
+from numpy import random
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(random.permutation(arr))
