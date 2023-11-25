7d76a73f176ac15f2a457b3599c913cdd0cb0ed0
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 13:23:33 +0300

    final commit

--- w3schools/numpy ufunc/simple arithmetic/1 example.py
+++ w3schools/numpy ufunc/simple arithmetic/1 example.py
@@ -78,3 +78,12 @@ arr2 = np.array([3, 7, 9, 8, 2, 33])
 newarr = np.divmod(arr1, arr2)
 
 print(newarr)
+
+
+import numpy as np
+
+arr = np.array([-1, -2, 1, 2, 3, -4])
+
+newarr = np.absolute(arr)
+
+print(newarr)
