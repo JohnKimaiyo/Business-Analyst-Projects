ad9459a7b47e9d9e922eb5f3f2885ad680d78f14
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 19:20:15 +0300

    final comit

--- w3schools/pandas/Series/1 example.py
+++ w3schools/pandas/Series/1 example.py
@@ -33,3 +33,15 @@ calories = {"day1": 420, "day2": 380, "day3": 390}
 myvar = pd.Series(calories, index = ["day1", "day2"])
 
 print(myvar)
+
+# example 5
+import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+myvar = pd.DataFrame(data)
+
+print(myvar)
