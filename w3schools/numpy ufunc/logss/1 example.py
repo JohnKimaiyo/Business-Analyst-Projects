4616c6fc10abae05eaa6f39efa612a35fe5ccd28
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-25 17:54:46 +0300

    finl commit

--- w3schools/numpy ufunc/logss/1 example.py
+++ w3schools/numpy ufunc/logss/1 example.py
@@ -19,3 +19,13 @@ import numpy as np
 nplog = np.frompyfunc(log, 2, 1)
 
 print(nplog(100, 15))
+
+# exapmle 4
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([1, 2, 3])
+
+newarr = np.add(arr1, arr2)
+
+print(newarr)
