92e7aa999e11212583c1697680a21bd9924f6eed
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-26 11:57:05 +0300

    final commit

---
+++ Main Projects/case study 1/Python Data Analysis/2 Data Exploration.py
@@ -0,0 +1,15 @@
+# Kmean Clustering
+# import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import plotly as py
+import plotly.graph_objs as go
+
+
+from sklearn.cluster import kmeans
+
+import warnings
+warnings.filterwarnings("ignore")
