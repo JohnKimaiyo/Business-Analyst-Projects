eca776aceeff2a548ed39bf6a9c1b1f2548de8a8
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-27 20:59:15 +0300

    final commit

--- Technical Interview/Python Algorith Technocal Interview/MERGE()/1 example.py
+++ Technical Interview/Python Algorith Technocal Interview/MERGE()/1 example.py
@@ -1,1 +1,11 @@
-print("hello")
+import pandas as pd
+
+# Creating two sample DataFrames
+df1 = pd.DataFrame({'key': ['A', 'B', 'C'], 'value': [1, 2, 3]})
+df2 = pd.DataFrame({'key': ['A', 'B', 'D'], 'value': [4, 5, 6]})
+
+# Merge the DataFrames on the 'key' column
+merged_df = pd.merge(df1, df2, on='key', how='inner')
+
+# Display the result
+print(merged_df)
