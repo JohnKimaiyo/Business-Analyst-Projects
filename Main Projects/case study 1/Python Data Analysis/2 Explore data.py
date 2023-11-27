21683678ae901f159b5a90fb8302367e94d8b3f2
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-27 14:44:40 +0300

    final commit

--- Main Projects/case study 1/Python Data Analysis/2 Explore data.py
+++ Main Projects/case study 1/Python Data Analysis/2 Explore data.py
@@ -82,6 +82,15 @@ y_min y_max = X2[:,1].min() - 1,X2[:,1].max() + 1
 xx, yy = np.meshgrid(np.c_[xx.ravel(),yyravel()])
 z2 = algorithm.predict(np.c_[xx.ravel(),yy.ravel()])
                        
-                       
+                    plt.figure(1,figsize = (15,7))
+plt.clf()
+z2 = z2.reshape(xx.shape)
+plt.imshow(z2, interpolation = "nearest",
+          extent = (xx.min(), xx.max(),yy.max()),
+           cmap = plt.cm.Paste12,aspect = "auto",origin ="lower")
+plt.scatter(x = "Annual Income(k$)", y =  "Spending Score (1-100)",data = df , c =labels2 , s = 100)
+plt.ylabel("Spending Score (1-100),plt.xlabel"),plt.xlabel("Annual Income(k$)")
+plt.show()   
+
 
 
