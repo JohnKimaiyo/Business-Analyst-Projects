8754037725b2108892eb56f6080cfe81010d3dbb
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-27 21:05:42 +0300

    final comit

---
+++ Technical Interview/Python Algorith Technocal Interview/UNIQUE/1 example.py
@@ -0,0 +1,12 @@
+# Define a list with duplicate elements
+my_list = [1, 2, 2, 3, 4, 4, 5, 6, 6]
+
+# Use set() to obtain unique elements
+unique_elements = set(my_list)
+
+# Convert the set back to a list if needed
+unique_list = list(unique_elements)
+
+# Print the original list and the list with unique elements
+print("Original List:", my_list)
+print("List with Unique Elements:", unique_list)
