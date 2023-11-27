61a03122bfae21bc47b00d62b905e8014f97cc3f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-27 15:35:56 +0300

    final commit

--- Main Projects/case study 1/Python Data Analysis/4 3D Clustering.py
+++ Main Projects/case study 1/Python Data Analysis/4 3D Clustering.py
@@ -14,7 +14,7 @@ from sklearn.cluster import KMeans
 import warnings
 warnings.filterwarnings("ignore")
 
-
+# step 1
 
 X3 = df[["Age","Annual Income (k$)","Spending Score"]]
 inertia = []
@@ -24,10 +24,26 @@ for n in range(1,11):
     algorithm.fit(x3)
     inertia.append(algorithm.inertia_)
 
+
+# step 2
+
 plt.figure(1,figsize = (15,6))
 plt.plot(np.arange(1,11), inertia, "0")
 plt.plot(np.arange(1,11),inertia,"-",alpha = 0.5)
 plt.ylabel("inertia")
 plt.show()
 
+
+#  step 3
+algorith = (KMeans (n_clusters = 6 ,init = "Kmeans++", n_init = 10,max_iter = 300;
+            tol = 0.001,random_state = 111, algorithm = "elkan"))
+algorithm.fit(X3)
+label3 = algorithm.labels_.
+centroids3 = algorithm.cluster_centers_
+
+
+y_kmeans = algorithm.fit_predict(X3)
+df["cluster"] = pd.DataFrame(y_kmeans)
+df.head()
+            
 
