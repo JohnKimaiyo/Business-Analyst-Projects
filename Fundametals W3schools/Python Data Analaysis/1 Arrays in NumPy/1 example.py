1f428117e46eddcb5deaea0151607b6c956de747
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-28 12:57:14 +0300

    final commit

--- Fundametals W3schools/Python Data Analaysis/1 Arrays in NumPy/1 example.py
+++ Fundametals W3schools/Python Data Analaysis/1 Arrays in NumPy/1 example.py
@@ -99,3 +99,22 @@ print(div_ans)
 div_ans = np.divide(a, b)
 print(div_ans)
 
+
+
+
+
+
+# NumPy Array Indexing
+# Python NumPy Array Indexing
+# Python program to demonstrate 
+# the use of index arrays.
+import numpy as np
+
+# Create a sequence of integers from
+# 10 to 1 with a step of -2
+a = np.arange(10, 1, -2) 
+print("\n A sequential array with a negative step: \n",a)
+
+# Indexes are specified inside the np.array method.
+newarr = a[np.array([3, 1, 2 ])]
+print("\n Elements at these indices are:\n",newarr)
