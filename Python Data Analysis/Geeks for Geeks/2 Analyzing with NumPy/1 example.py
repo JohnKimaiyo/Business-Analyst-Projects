c2df56da78d184232ff70d95045284b870563ff7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-28 17:20:50 +0300

    final commit

--- Python Data Analysis/Geeks for Geeks/2 Analyzing with NumPy/1 example.py
+++ Python Data Analysis/Geeks for Geeks/2 Analyzing with NumPy/1 example.py
@@ -53,3 +53,18 @@ print(add_ans)
 
 add_ans = np.add(a,b,c)
 print(add_ans)      
+
+ # subtraction
+ import numpy as np
+ 
+ # defining both the matrices
+ a = np.array([5,72,13,100])
+b= np.array([2,5,10,30])
+
+# performing subtraction using arithmetci operator
+sub_ans = a-b
+print(sub_ans)
+
+# perfomring subtraction using numpy function
+sub_ans = np.subtract(a,b)
+print(sub_ans)
