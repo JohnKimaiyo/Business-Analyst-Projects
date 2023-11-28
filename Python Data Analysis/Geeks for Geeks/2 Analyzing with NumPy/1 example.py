7baaeae1901fa0299bdc8598ac1969e59f328103
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-28 13:51:42 +0300

    final comit

--- Python Data Analysis/Geeks for Geeks/2 Analyzing with NumPy/1 example.py
+++ Python Data Analysis/Geeks for Geeks/2 Analyzing with NumPy/1 example.py
@@ -1,2 +1,11 @@
 # creating an array
 import numpy as np
+
+b = np.empty(2,dtype = int)
+print("Matrix b:\n",b)
+
+a = np.empty([2,2],dtype = int)
+print("\nMatrix b:\n",a)
+
+c = np.empty([3,3])
+print("\nMatrix c: \n",c)
