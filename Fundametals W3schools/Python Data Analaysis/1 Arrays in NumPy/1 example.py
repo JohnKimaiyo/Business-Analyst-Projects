7c02d9efd942f973c985f68dbc451ade1295f405
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-28 13:03:10 +0300

    final commit

--- Fundametals W3schools/Python Data Analaysis/1 Arrays in NumPy/1 example.py
+++ Fundametals W3schools/Python Data Analaysis/1 Arrays in NumPy/1 example.py
@@ -118,3 +118,18 @@ print("\n A sequential array with a negative step: \n",a)
 # Indexes are specified inside the np.array method.
 newarr = a[np.array([3, 1, 2 ])]
 print("\n Elements at these indices are:\n",newarr)
+
+
+# NumPy Array Slicing
+# Python program for basic slicing.
+import numpy as np
+
+# Arrange elements from 0 to 19
+a = np.arange(20)
+print("\n Array is:\n ",a)
+
+# a[start:stop:step]
+print("\n a[-8:17:1] = ",a[-8:17:1])
+
+# The : operator means all elements till the end.
+print("\n a[10:] = ",a[10:])
