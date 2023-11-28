9c2898d2288831ab4191cd58828fc31b87315dc2
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-28 12:44:12 +0300

    final commit

--- Fundametals W3schools/Python Data Analaysis/1 Arrays in NumPy/1 example.py
+++ Fundametals W3schools/Python Data Analaysis/1 Arrays in NumPy/1 example.py
@@ -51,3 +51,19 @@ print(add_ans)
 
 add_ans = np.add(a, b, c)
 print(add_ans)
+
+
+#Subtraction:
+import numpy as np
+
+# Defining both the matrices
+a = np.array([5, 72, 13, 100])
+b = np.array([2, 5, 10, 30])
+
+# Performing subtraction using arithmetic operator
+sub_ans = a-b
+print(sub_ans)
+
+# Performing subtraction using numpy function
+sub_ans = np.subtract(a, b)
+print(sub_ans)
