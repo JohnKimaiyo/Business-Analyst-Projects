c0aad5047a71b574a3f3273fbcca08c4022a9e3e
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-28 12:29:36 +0300

    final comit

--- Fundametals W3schools/Python Data Analaysis/Matplotlib/1 example.py
+++ Fundametals W3schools/Python Data Analaysis/Matplotlib/1 example.py
@@ -21,3 +21,20 @@ import matplotlib.pyplot as plt
 plt.plot([1,2,3,4], [1,4,9,10])
 plt.axis([0,6,0,20])
 plt.show()
+
+
+# Python program to show pyplot module 
+import matplotlib.pyplot as plt 
+from matplotlib.figure import Figure 
+
+# Creating a new figure with width = 5 inches 
+# and height = 4 inches 
+fig = plt.figure(figsize =(5, 4)) 
+
+# Creating a new axes for the figure 
+ax = fig.add_axes([1, 1, 1, 1]) 
+
+# Adding the data to be plotted 
+ax.plot([2, 3, 4, 5, 5, 6, 6], 
+		[5, 7, 1, 3, 4, 6 ,8]) 
+plt.show()
