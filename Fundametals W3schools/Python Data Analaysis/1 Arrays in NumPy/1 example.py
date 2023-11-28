7422fb6658f143af04e0d4ccd05442ae39d12428
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-28 13:06:22 +0300

    final commit

--- Fundametals W3schools/Python Data Analaysis/1 Arrays in NumPy/1 example.py
+++ Fundametals W3schools/Python Data Analaysis/1 Arrays in NumPy/1 example.py
@@ -143,3 +143,29 @@ b = np.array([[[1, 2, 3],[4, 5, 6]],
 			[[7, 8, 9],[10, 11, 12]]])
 
 print(b[...,1]) #Equivalent to b[: ,: ,1 ]
+
+# NumPy Array Broadcasting
+import numpy as np
+
+macros = np.array([
+[0.8, 2.9, 3.9],
+[52.4, 23.6, 36.5],
+[55.2, 31.7, 23.9],
+[14.4, 11, 4.9]
+])
+
+# Create a new array filled with zeros,
+# of the same shape as macros.
+result = np.zeros_like(macros)
+
+cal_per_macro = np.array([3, 3, 8])
+
+# Now multiply each row of macros by
+# cal_per_macro. In Numpy, `*` is
+# element-wise multiplication between two arrays.
+for i in range(macros.shape[0]):
+	result[i, :] = macros[i, :] * cal_per_macro
+
+result
+
+
