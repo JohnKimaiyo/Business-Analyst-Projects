9a859cd287319f831ffc75ca904fdac14d9d73ad
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-28 17:15:26 +0300

    final commit

--- Python Data Analysis/Geeks for Geeks/2 Analyzing with NumPy/1 example.py
+++ Python Data Analysis/Geeks for Geeks/2 Analyzing with NumPy/1 example.py
@@ -21,4 +21,35 @@ print("\nMatrix a: \n",a)
 
 c = np.zeros([3,3])
 print('\nMatrix c:\n',c)
-      
+
+
+# Operations on Numpy 
+
+# Aritmetic Operations
+
+# Additon
+
+import numpy as np
+
+# Defining both the matrices
+
+a = np.array([5,72,13,100])
+b= np.array([2,5,10,30])
+
+# Perfomring addition using arithmetic operaator
+add_ans = a + b
+print(add_ans)
+
+# Performing addition using numpy function
+add_ans = np.add(a,b)
+print(add_ans)
+
+# The same functions and operations can be used for
+# muliple matrices
+
+c = np.array([1,2,3,4])
+add_ans = a + b + c
+print(add_ans)
+
+add_ans = np.add(a,b,c)
+print(add_ans)      
