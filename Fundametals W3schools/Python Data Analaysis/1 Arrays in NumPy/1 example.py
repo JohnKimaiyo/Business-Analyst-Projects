15e45671dfe31bb60e45a609654825bc96639af1
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-28 12:45:54 +0300

    final commit

--- Fundametals W3schools/Python Data Analaysis/1 Arrays in NumPy/1 example.py
+++ Fundametals W3schools/Python Data Analaysis/1 Arrays in NumPy/1 example.py
@@ -84,3 +84,18 @@ print(mul_ans)
 mul_ans = np.multiply(a, b)
 print(mul_ans)
 
+# Division:
+import numpy as np
+
+# Defining both the matrices
+a = np.array([5, 72, 13, 100])
+b = np.array([2, 5, 10, 30])
+
+# Performing division using arithmetic operators
+div_ans = a/b
+print(div_ans)
+
+# Performing division using numpy functions
+div_ans = np.divide(a, b)
+print(div_ans)
+
