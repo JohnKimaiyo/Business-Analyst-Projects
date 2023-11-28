f60c9e4575492c04aa8ba91ce38c3675915e2512
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-28 13:04:58 +0300

    final commit

--- Fundametals W3schools/Python Data Analaysis/1 Arrays in NumPy/1 example.py
+++ Fundametals W3schools/Python Data Analaysis/1 Arrays in NumPy/1 example.py
@@ -133,3 +133,13 @@ print("\n a[-8:17:1] = ",a[-8:17:1])
 
 # The : operator means all elements till the end.
 print("\n a[10:] = ",a[10:])
+
+
+# Python program for indexing using basic slicing with ellipsis
+import numpy as np
+
+# A 3 dimensional array.
+b = np.array([[[1, 2, 3],[4, 5, 6]],
+			[[7, 8, 9],[10, 11, 12]]])
+
+print(b[...,1]) #Equivalent to b[: ,: ,1 ]
