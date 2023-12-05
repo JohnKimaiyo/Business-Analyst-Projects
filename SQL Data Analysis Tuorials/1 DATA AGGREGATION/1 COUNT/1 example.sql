d07db03bdf064a98db9f0696ad42c5765e69e558
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-12-05 10:56:09 +0300

    final commit

--- SQL Data Analysis Tuorials/1 DATA AGGREGATION/1 COUNT/1 example.sql
+++
@@ -1,4 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT COUNT([MaritalStatus])  AS TotalNumberofMarried_People
-  FROM [AdventureWorks2019].[HumanResources].[Employee]
-  WHERE [MaritalStatus] = 'M'
--- SQL Data Analysis Tuorials/1 DATA AGGREGATION/2 SUM/1 example.sql
+++
@@ -1,3 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT SUM([VacationHours]) AS Total_Vacation_Hours
-  FROM [AdventureWorks2019].[HumanResources].[Employee]
